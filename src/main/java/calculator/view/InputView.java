6d14468da0f8df6f43db75455208e82ef79fe5a5
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-14 21:38:05 +0900

    feat: 공백 문자가 포함된 입력 시 예외 발생

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,13 +3,20 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String WHITESPACE = " ";
 
     public String inputString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         validateBlank(input);
+        validateWhitespace(input);
         return input;
+    }
+
+    private void validateWhitespace(String input) {
+        if (input.contains(WHITESPACE))
+            throw new IllegalArgumentException("유효하지 않은 입력입니다.");
     }
 
     private void validateBlank(String input) {
