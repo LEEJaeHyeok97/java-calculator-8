f8d1949c2de8b0eed60319f21139a339ca1dc654
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-16 02:17:43 +0900

    feat: PositiveNumber 생성 시 양수 값 검증 기능 추가

--- src/main/java/calculator/model/PositiveNumber.java
+++ src/main/java/calculator/model/PositiveNumber.java
@@ -2,13 +2,26 @@ package calculator.model;
 
 public class PositiveNumber {
 
+    public static final String NON_POSITIVE_NUMBER = "양수를 입력해야 합니다.";
+    public static final long MIN_RANGE = 1L;
     private final long number;
 
     public PositiveNumber(long number) {
+        validateRange(number);
         this.number = number;
     }
 
     public long getNumber() {
         return number;
+    }
+
+    private void validateRange(long number) {
+        if (isOutOfRange(number)) {
+            throw new IllegalArgumentException(NON_POSITIVE_NUMBER);
+        }
+    }
+
+    private boolean isOutOfRange(long number) {
+        return number < MIN_RANGE;
     }
 }
