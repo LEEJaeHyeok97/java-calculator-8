a70d9273538bcaf7404b39a954109e5880853bec
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-18 00:53:13 +0900

    feat: 커스텀 구분자와 기본 구분자 외 다른 구분자를 입력한 경우 예외 처리

--- src/main/java/calculator/model/PositiveNumber.java
+++ src/main/java/calculator/model/PositiveNumber.java
@@ -4,6 +4,7 @@ public class PositiveNumber {
 
     public static final String NON_POSITIVE_NUMBER = "양수를 입력해야 합니다.";
     public static final long MIN_RANGE = 1L;
+    public static final String INVALID_DELIMITER_EXCEPTION_MESSAGE = "구분자가 잘못 입력 되었습니다. 커스텀 구분자, 기본 구분자를 확인하세요";
     private final long number;
 
     public PositiveNumber(long number) {
@@ -11,16 +12,28 @@ public class PositiveNumber {
         this.number = number;
     }
 
+    public long getNumber() {
+        return number;
+    }
+
     public static PositiveNumber from(String positiveNumber) {
         return new PositiveNumber(getParsedLong(positiveNumber));
     }
 
     private static long getParsedLong(String positiveNumber) {
-        return Long.parseLong(positiveNumber);
+        return toLong(positiveNumber);
     }
 
-    public long getNumber() {
-        return number;
+    private static long toLong(String positiveNumber) {
+        return parseLong(positiveNumber);
+    }
+
+    private static long parseLong(String positiveNumber) {
+        try {
+            return Long.parseLong(positiveNumber);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(INVALID_DELIMITER_EXCEPTION_MESSAGE);
+        }
     }
 
     private void validateRange(long number) {
